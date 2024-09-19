6b7022fb9c98d2acd1afadc0b2879d882ba64ff9
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 13:33:31 +0100

    pushchange

--- pagetransition.js
+++ pagetransition.js
@@ -23,7 +23,7 @@ links.forEach((link) => {
   link.addEventListener("click", function (e) {
     if (
       this.hostname === window.location.hostname &&
-      this.href.indexOf("#") === -1 &&
+      
       this.getAttribute("target") !== "_blank"
     )
       e.preventDefault();
