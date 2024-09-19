667fb2ca519d96c7fab994b4f721bdf2d04ec365
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 15:49:22 +0100

    push change

--- pagetransition.js
+++ pagetransition.js
@@ -10,22 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
   
     const links = document.querySelectorAll("a");
     links.forEach((link) => {
-      PageIn
-      .from(mainwrap, {
-        y: "50vh",
-        duration: 1.25,
-        ease: "expo.inOut",
-      })
-      .to(pageTransitionItems, {
-        y: "-100vh",
-        duration: 1,
-        ease: "expo.inOut",
-        stagger: {
-          amount: 0.1,
-          from: "random",
-        },
-      }, '<');
+      
       link.addEventListener("click", function (e) {
+        
         if (
           this.hostname === window.location.hostname &&
           this.href.indexOf("#") === -1 &&
@@ -52,6 +39,21 @@ document.addEventListener("DOMContentLoaded", function () {
                 from: "random",
               },
               onComplete: () => {
+                PageIn
+                .from(mainwrap, {
+                  y: "50vh",
+                  duration: 1.25,
+                  ease: "expo.inOut",
+                })
+                .to(pageTransitionItems, {
+                  y: "-100vh",
+                  duration: 1,
+                  ease: "expo.inOut",
+                  stagger: {
+                    amount: 0.1,
+                    from: "random",
+                  },
+                }, '<');
                 window.location = destination;
               },
             }
