39ad0f0286ef5cdeaf9ca80f98daf07a363006e4
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 10:57:22 +0100

    push achange

--- pagetransition.js
+++ pagetransition.js
@@ -1,40 +1,36 @@
 const pageTransition = document.querySelector(".new-page-transition");
-const pageTransitionItems = document.querySelectorAll(".new-page-transition_item");
+const pageTransitionItems = document.querySelectorAll(
+  ".new-page-transition_item"
+);
 
 const links = document.querySelectorAll("a");
 links.forEach((link) => {
-link.addEventListener("click", function (e) {
-if (
-    this.hostname === window.location.hostname &&
-this.href.indexOf("#") === -1 &&
-this.getAttribute("target") !== "_blank"
-)
-
-e.preventDefault();
-let destination = this.href;
-pageTransition.style.display = "grid";
-gsap.fromTo(
-    pageTransitionItems,
-    {
-y: "100vh",
-    },
-    {
-y: "Ovh",
-duration: 1,
-ease: "expo.inOut",
-stagger: {
-    amount: 0.1,
-from: "random",
-},
-onComplete: () => {
-    window.location = destination
-}
-    }
-
-
-);
+  link.addEventListener("click", function (e) {
+    if (
+      this.hostname === window.location.hostname &&
+      this.href.indexOf("#") === -1 &&
+      this.getAttribute("target") !== "_blank"
+    )
+      e.preventDefault();
+    let destination = this.href;
+    pageTransition.style.display = "grid";
+    gsap.fromTo(
+      pageTransitionItems,
+      {
+        y: "100vh",
+      },
+      {
+        y: "Ovh",
+        duration: 1,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.1,
+          from: "random",
+        },
+        onComplete: () => {
+          window.location = destination;
+        },
+      }
+    );
+  });
 });
-});
-
-
-
