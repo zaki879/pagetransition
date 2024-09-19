eaac5b88d0e56569409a18979293c951b875faf6
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 14:06:26 +0100

    pushchange

--- pagetransition.js
+++ pagetransition.js
@@ -18,34 +18,39 @@ PageIn
       from: "random",
     },
   },'<');
+
 const links = document.querySelectorAll("a");
 links.forEach((link) => {
   link.addEventListener("click", function (e) {
+    // Allow transition for all links except _blank and same-page hashes
     if (
       this.hostname === window.location.hostname &&
-      this.href.indexOf("#") === -1 &&
+      !this.href.includes("#") && // Remove this condition if you want hash links to also trigger
       this.getAttribute("target") !== "_blank"
-    )
+    ) {
       e.preventDefault();
-    let destination = this.href;
-    pageTransition.style.display = "grid";
-    PageIn.fromTo(
-      pageTransitionItems,
-      {
-        y: "100vh",
-      },
-      {
-        y: "0vh",
-        duration: 1,
-        ease: "expo.inOut",
-        stagger: {
-          amount: 0.1,
-          from: "random",
+      let destination = this.href;
+
+      // Trigger page transition animation
+      pageTransition.style.display = "grid";
+      PageIn.fromTo(
+        pageTransitionItems,
+        {
+          y: "100vh",
         },
-        onComplete: () => {
-          window.location = destination;
-        },
-      }
-    );
+        {
+          y: "0vh",
+          duration: 1,
+          ease: "expo.inOut",
+          stagger: {
+            amount: 0.1,
+            from: "random",
+          },
+          onComplete: () => {
+            window.location = destination;
+          },
+        }
+      );
+    }
   });
 });
