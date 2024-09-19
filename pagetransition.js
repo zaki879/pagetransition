3d7fd548dd49108a0d43c7bcce4a27bdac10cca0
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 15:45:12 +0100

    push change

--- pagetransition.js
+++ pagetransition.js
@@ -5,7 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Only run the animation if these elements exist
   if (mainwrap && pageTransition && pageTransitionItems.length > 0) {
-   
+    const PageIn = gsap.timeline();
+    PageIn
+      .from(mainwrap, {
+        y: "50vh",
+        duration: 1.25,
+        ease: "expo.inOut",
+      })
+      .to(pageTransitionItems, {
+        y: "-100vh",
+        duration: 1,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.1,
+          from: "random",
+        },
+      }, '<');
   
     const links = document.querySelectorAll("a");
     links.forEach((link) => {
@@ -20,9 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
           // Show the transition
           pageTransition.style.display = "grid";
-      
+
           // Animate the transition
-          gsap.fromTo(
+          PageIn.fromTo(
             pageTransitionItems,
             {
               y: "100vh",
@@ -36,9 +51,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 from: "random",
               },
               onComplete: () => {
-                console.log('====================================');
-                console.log(destination);
-                console.log('====================================');
                 window.location = destination;
               },
             }
