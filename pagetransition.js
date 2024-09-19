716ee6c79cf587dcdca0e805cce8c6a1e69e27d1
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 11:41:44 +0100

    push change

--- pagetransition.js
+++ pagetransition.js
@@ -1,8 +1,33 @@
+
+const mainwrap =document.querySelector('.main-wrapper')
 const pageTransition = document.querySelector(".new-page-transition");
 const pageTransitionItems = document.querySelectorAll(
   ".hero-page-transition_item"
 );
 
+const tlPageIn = gsap.timeline();
+tlPageIn
+.from(mainwrap, {
+     y: "50vh",
+     duration: 1.25,
+ease: "expo.inOut",
+})
+
+.to(pageTransitionItems, {
+y: '-100vh',
+duration: 1,
+ease: 'expo.inOut',
+ stagger: {
+    amount: 0.1,
+from: 'random'
+},
+
+onComplete: () =>{
+    pageTransition.style.display = 'none'
+
+}
+
+});
 const links = document.querySelectorAll("a");
 links.forEach((link) => {
   link.addEventListener("click", function (e) {
@@ -20,7 +45,7 @@ links.forEach((link) => {
         y: "100vh",
       },
       {
-        y: "Ovh",
+        y: "0vh",
         duration: 1,
         ease: "expo.inOut",
         stagger: {
