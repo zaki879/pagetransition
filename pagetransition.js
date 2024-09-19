75ab3b43a38d2e8ce25cade258a10b6118a5c9bf
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 14:10:48 +0100

    push change

--- pagetransition.js
+++ pagetransition.js
@@ -10,7 +10,7 @@ PageIn
     ease: "expo.inOut",
   })
   .to(pageTransitionItems, {
-    y: "-100vh",
+    y: "0vh",
     duration: 1,
     ease: "expo.inOut",
     stagger: {
@@ -18,39 +18,34 @@ PageIn
       from: "random",
     },
   },'<');
-
 const links = document.querySelectorAll("a");
 links.forEach((link) => {
   link.addEventListener("click", function (e) {
-    // Allow transition for all links except _blank and same-page hashes
     if (
       this.hostname === window.location.hostname &&
-      !this.href.includes("#") && // Remove this condition if you want hash links to also trigger
+      this.href.indexOf("#") === -1 &&
       this.getAttribute("target") !== "_blank"
-    ) {
+    )
       e.preventDefault();
-      let destination = this.href;
-
-      // Trigger page transition animation
-      pageTransition.style.display = "grid";
-      PageIn.fromTo(
-        pageTransitionItems,
-        {
-          y: "100vh",
+    let destination = this.href;
+    pageTransition.style.display = "grid";
+    PageIn.fromTo(
+      pageTransitionItems,
+      {
+        y: "100vh",
+      },
+      {
+        y: "0vh",
+        duration: 1,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.1,
+          from: "random",
         },
-        {
-          y: "0vh",
-          duration: 1,
-          ease: "expo.inOut",
-          stagger: {
-            amount: 0.1,
-            from: "random",
-          },
-          onComplete: () => {
-            window.location = destination;
-          },
-        }
-      );
-    }
+        onComplete: () => {
+          window.location = destination;
+        },
+      }
+    );
   });
 });
