731cf89eb5f407c645df880b200ed5131d14e0ce
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 13:44:16 +0100

    pushchange

--- pagetransition.js
+++ pagetransition.js
@@ -25,29 +25,27 @@ links.forEach((link) => {
       this.hostname === window.location.hostname &&
       this.href.indexOf("#") === -1 &&
       this.getAttribute("target") !== "_blank"
-    ){
+    )
       e.preventDefault();
-      let destination = this.href;
-      pageTransition.style.display = "grid";
-      gsap.fromTo(
-        pageTransitionItems,
-        {
-          y: "100vh",
+    let destination = this.href;
+    pageTransition.style.display = "grid";
+    PageIn.fromTo(
+      pageTransitionItems,
+      {
+        y: "100vh",
+      },
+      {
+        y: "0vh",
+        duration: 1,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.1,
+          from: "random",
         },
-        {
-          y: "0vh",
-          duration: 1,
-          ease: "expo.inOut",
-          stagger: {
-            amount: 0.1,
-            from: "random",
-          },
-          onComplete: () => {
-            window.location = destination;
-          },
-        }
-      );
-    }
-  
+        onComplete: () => {
+          window.location = destination;
+        },
+      }
+    );
   });
 });
