fe11112b84b7461f5e27286633ac0739ff812f98
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-19 15:31:20 +0100

    qds

--- pagetransition.js
+++ pagetransition.js
@@ -1,72 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function runPageTransitionAnimation() {
-    const mainwrap = document.querySelector(".main-wrapper");
-    const pageTransition = document.querySelector(".new-page-transition");
-    const pageTransitionItems = document.querySelectorAll(".hero-page-transition_item");
+  const mainwrap = document.querySelector(".main-wrapper");
+  const pageTransition = document.querySelector(".new-page-transition");
+  const pageTransitionItems = document.querySelectorAll(".hero-page-transition_item");
 
-    // Only run the animation if these elements exist
-    if (mainwrap && pageTransition && pageTransitionItems.length > 0) {
-      const PageIn = gsap.timeline();
-      PageIn
-        .from(mainwrap, {
-          y: "50vh",
-          duration: 0.25,
-          ease: "expo.inOut",
-        })
-        .to(pageTransitionItems, {
-          y: "-100vh",
-          duration: 1,
-          ease: "expo.inOut",
-          stagger: {
-            amount: 0.1,
-            from: "random",
-          },
-        }, '<');
+  // Only run the animation if these elements exist
+  if (mainwrap && pageTransition && pageTransitionItems.length > 0) {
+    const PageIn = gsap.timeline();
+    PageIn
+      .from(mainwrap, {
+        y: "50vh",
+        duration: 1.25,
+        ease: "expo.inOut",
+      })
+      .to(pageTransitionItems, {
+        y: "-100vh",
+        duration: 1,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.1,
+          from: "random",
+        },
+      }, '<');
+  
+    const links = document.querySelectorAll("a");
+    links.forEach((link) => {
+      link.addEventListener("click", function (e) {
+        if (
+          this.hostname === window.location.hostname &&
+          this.href.indexOf("#") === -1 &&
+          this.getAttribute("target") !== "_blank"
+        ) {
+          e.preventDefault();
+          let destination = this.href;
 
-      const links = document.querySelectorAll("a");
-      links.forEach((link) => {
-        link.addEventListener("click", function (e) {
-          if (
-            this.hostname === window.location.hostname &&
-            this.href.indexOf("#") === -1 &&
-            this.getAttribute("target") !== "_blank"
-          ) {
-            e.preventDefault();
-            let destination = this.href;
+          // Show the transition
+          pageTransition.style.display = "grid";
 
-            // Show the transition
-            pageTransition.style.display = "grid";
-
-            // Animate the transition
-            gsap.fromTo(
-              pageTransitionItems,
-              {
-                y: "100vh",
+          // Animate the transition
+          gsap.fromTo(
+            pageTransitionItems,
+            {
+              y: "100vh",
+            },
+            {
+              y: "0vh",
+              duration: 2,
+              ease: "expo.inOut",
+              stagger: {
+                amount: 0.1,
+                from: "random",
               },
-              {
-                y: "0vh",
-                duration: 2,
-                ease: "expo.inOut",
-                stagger: {
-                  amount: 0.1,
-                  from: "random",
-                },
-                onComplete: () => {
-                  window.location = destination;
-                },
-              }
-            );
-          }
-        });
+              onComplete: () => {
+                console.log('====================================');
+                console.log(destination);
+                console.log('====================================');
+                window.location = destination;
+              },
+            }
+          );
+        }
       });
-    }
+    });
   }
-
-  // Run the function initially
-  runPageTransitionAnimation();
-
-  // Use Webflow's push method to re-run the function after page transitions
-  Webflow.push(function () {
-    runPageTransitionAnimation();
-  });
 });
